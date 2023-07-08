e9ab6bea2eb5626dccc1fef2ce9d9741a010bbc4
Safronov Alexander <sasha.safronov1996161681@gmail.com>
2023-07-08 17:31:13 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let manyText = document.querySelector(".many__text");
+if (window.innerWidth < 991) {
+  manyText.textContent = `Finding highly vetted college babysitters and nannies should be easy, affordable and take seconds.
+    This is the mantra we have followed since the very first day. We also knew we wanted to create an environment
+    that fostered trust and safety, which is why every Wyndy babsysitter and nanny undergoes an interview by a Wyndy team member,
+    extensive background check and are verified as enrolled students by the National Student Clearinghouse.`;
+}
